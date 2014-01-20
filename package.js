9319eed525aa6093835fc5ad93b431168f3245f5
Pauli B <pauli.b@outlook.com>
2014-01-20 18:41:54 +0000

    Update package.js

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 
 Package.on_use(function(api) {
   api.use(['underscore', 'deps', 'templating', 'handlebars', 'moment'], 'client');
-  api.use('collection2', ['client', 'server'], {weak: true});
+  // api.use('collection2', ['client', 'server'], {weak: true});
   api.use('simple-schema', ['client', 'server']);
 
   if (typeof api.export !== 'undefined') {
